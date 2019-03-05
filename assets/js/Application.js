0e40259dd00bf0bf2f95561b60d3e11ddb6e3e1a
Olivier Decock <olivdeco1@arteveldehs.be>
2019-03-05 15:01:48 +0100

    v0.2.0
    Voorlopig

--- assets/js/Application.js
+++ assets/js/Application.js
@@ -1,26 +1,42 @@
-import Vector2 from "../Math/Vector2.js"
+import Canvas from './Library/Canvas.js'
+import Tests from './Tests/Tests.js'
 
-export default class Application
-{
-    constructor()
-    {
+/** Class for the application. */
+export default class Application {
+    /**
+     * Create a new application.
+     */
+    constructor() {
+        const tests = false
+        if (tests) {
+            new Tests()
+        }
         console.info('WebGL2 Demo')
-        const v1 = new Vector2
-        const v2 = new Vector2(1.5, 2.5)
-        console.log('start:', v1, v2)
 
-        v1.sub(v2)
-        console.log(v1, v2)
-        
-        v1.scalar(v2)
-        console.log(v1, v2)
+        this.shaderSources = {
+            fragment: null,
+            vertex: null,
+        }
+        this.preloader()
+    }
 
-        console.log('norm of v1:', v1.norm())
-        const v3 = new Vector2(3,5)
-        const v4 = new Vector2(5,3)
-        const v5 = new Vector2(-3, -5)
-        
-        console.log('v3 . v4:', v3.dot(v4))
-        console.log('v5 . v6:', v3.dot(v5))
+    async preloader() {
+        console.info('Preloading source code for shaders')
+        await fetch('./assets/glsl/vertex-shader.glsl')
+            .then(response => response.text())
+            .then(source => this.shaderSources.vertex = source)
+            .catch(error => console.error(error.message))
+        await fetch('./assets/glsl/fragment-shader.glsl')
+            .then(response => response.text())
+            .then(source => this.shaderSources.fragment = source)
+            .catch(error => console.error(error.message))
+        this.run()
+    }
+
+    run() {
+        const width = 600
+        const height = 480
+
+        new Canvas(width, height, this.shaderSources)
     }
 }
